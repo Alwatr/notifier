4ae9d532db54e0d7cd4f0ac2c807efd8f16dc410
Mohammad Honarvar <honarvar.info@gmail.com>
2024-10-21 23:39:20 +0330

    refactor(telegram): types

--- packages/telegram/src/type.ts
+++ packages/telegram/src/type.ts
@@ -1,1 +1,16 @@
-export type Group = {members: number[]}
+import type {Chat} from 'grammy/types';
+
+declare global {
+  type GroupItem = {
+    // title: string;
+    memberList: Member[];
+  }
+
+  type Member = {
+    id: string;
+    type: Chat['type'],
+    username?: string;
+    first_name?: string;
+    last_name?: string;
+  }
+}
