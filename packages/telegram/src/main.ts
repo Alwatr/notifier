f1b11863665184f1d50bd8d2542fbf8b5d86b105
Mohammad Honarvar <honarvar.info@gmail.com>
2024-10-21 23:41:34 +0330

    fix(telegram): start `bot`

--- packages/telegram/src/main.ts
+++ packages/telegram/src/main.ts
@@ -4,14 +4,14 @@
 import './bot/start-command.js';
 import './bot/unsubscribe-callback.js'
 import {bot} from './lib/bot.js';
-import {logger} from './lib/config.js';
+import {config, logger} from './lib/config.js';
 import {initializeNitrobase} from './lib/initialize-nitrobase.js';
 import './route/home.js'
 import './route/notify.js'
 
 logger.banner('@alwatr/telegram-notifier');
 
-// bot.start({onStart, drop_pending_updates: config.bot.dropPendingUpdates});
+bot.start({drop_pending_updates: config.bot.dropPendingUpdates});
 
 bot.catch(async (err: { message: unknown; }) => {
   logger.error('catch', 'catch_error', err);
