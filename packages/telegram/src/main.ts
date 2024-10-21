66d4297414bfa9e36aaaf9e3867f82077905eb56
Mohammad Honarvar <honarvar.info@gmail.com>
2024-10-22 00:46:13 +0330

    fix(telegram): add a missing `import` & format

--- packages/telegram/src/main.ts
+++ packages/telegram/src/main.ts
@@ -1,13 +1,14 @@
 // common imports
 // import {notifyDebug, onStart} from './lib/admin.js';
 // bot imports
+import './bot/new-group-command.js';
 import './bot/start-command.js';
-import './bot/unsubscribe-callback.js'
+import './bot/unsubscribe-callback.js';
 import {bot} from './lib/bot.js';
 import {config, logger} from './lib/config.js';
 import {initializeNitrobase} from './lib/initialize-nitrobase.js';
-import './route/home.js'
-import './route/notify.js'
+import './route/home.js';
+import './route/notify.js';
 
 logger.banner('@alwatr/telegram-notifier');
 
