9aec8917a60368301f927ff4f4ae0b1b555b1cb7
Mohammad Honarvar <honarvar.info@gmail.com>
2024-10-22 16:11:17 +0330

    chore(notifier-api): cleanup

--- packages/notifier-api/src/main.ts
+++ packages/notifier-api/src/main.ts
@@ -1,22 +1,11 @@
-// common imports
-// import {notifyDebug, onStart} from './lib/admin.js';
-// bot imports
 import './bot/new-category-command.js';
 import './bot/start-command.js';
 import './bot/unsubscribe-callback.js';
-import {bot} from './lib/bot.js';
-import {config, logger} from './lib/config.js';
+import {logger} from './lib/config.js';
 import {initializeNitrobase} from './lib/initialize-nitrobase.js';
 import './route/home.js';
 import './route/notify.js';
 
 logger.banner('@alwatr/telegram-notifier');
-
-bot.start({drop_pending_updates: config.bot.dropPendingUpdates});
-
-bot.catch(async (err: { message: unknown; }) => {
-  logger.error('catch', 'catch_error', err);
-  // await notifyDebug(`❗️ [#catch_error]: ${err.message}`);
-});
 
 initializeNitrobase();
