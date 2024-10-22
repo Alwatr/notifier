572fe39d33a9c7ec0a47d00618fb2a6f6c9a9b43
Mohammad Honarvar <honarvar.info@gmail.com>
2024-10-22 16:09:31 +0330

    feat(notifier-api): use `hashGenerator` & save data

--- packages/notifier-api/src/bot/new-category-command.ts
+++ packages/notifier-api/src/bot/new-category-command.ts
@@ -1,6 +1,6 @@
 import {bot} from '../lib/bot.js';
 import {config, logger} from '../lib/config.js';
-import {cryptoFactory} from '../lib/crypto.js';
+import {hashGenerator} from '../lib/hash.js';
 import {message} from '../lib/i18n.js';
 import {alwatrNitrobase} from '../lib/nitrobase.js';
 
@@ -12,12 +12,12 @@ import {alwatrNitrobase} from '../lib/nitrobase.js';
 bot.command('new_category', async (ctx) => {
   const chatId = ctx.chat.id;
   const categoryNameQueryParam = ctx.match;
-  logger.logMethodArgs?.('new_category', {chatId, groupNameQueryParam: categoryNameQueryParam});
+  logger.logMethodArgs?.('new_category', {chatId, categoryNameQueryParam});
 
-  if (chatId !== config.bot.adminChatId) {
-    ctx.reply(message('commandAccessDenied'));
-    return;
-  }
+  // if (chatId !== config.bot.adminChatId) {
+  //   ctx.reply(message('commandAccessDenied'));
+  //   return;
+  // }
 
   const categoryName = categoryNameQueryParam.split('?name=')[1];
   if (!categoryName) {
@@ -26,11 +26,12 @@ bot.command('new_category', async (ctx) => {
   }
 
   const categoriesCollection = await alwatrNitrobase.openCollection<CategoryItem>(config.nitrobase.categoriesCollection);
-  const newCategoryId = cryptoFactory.generateUserId();
+  const newCategoryId = hashGenerator.generateSelfValidate(categoryName);
   categoriesCollection.addItem(newCategoryId, {
     categoryName,
     memberList: []
   });
+  categoriesCollection.save();
 
   ctx.reply(`https://t.me/${config.bot.info.username}?start=${newCategoryId}`);
 });
