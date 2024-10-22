3f24b61a079dd3dbfce79186f33987c6a4e4229e
Mohammad Honarvar <honarvar.info@gmail.com>
2024-10-22 16:10:47 +0330

    feat(notifier-api): start the bot & handle errors

--- packages/notifier-api/src/lib/bot.ts
+++ packages/notifier-api/src/lib/bot.ts
@@ -1,5 +1,12 @@
 import {Bot} from 'grammy';
 
-import {config} from './config.js';
+import {config, logger} from './config.js';
 
 export const bot = new Bot(config.bot.token, {botInfo: config.bot.info});
+
+bot.start({drop_pending_updates: config.bot.dropPendingUpdates});
+
+bot.catch(async (err: { message: unknown; }) => {
+  logger.error('catch', 'catch_error', err);
+  // await notifyDebug(`❗️ [#catch_error]: ${err.message}`);
+});
