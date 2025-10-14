42da9ad02988b24d732aeee6913a5c747ff64f38
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-10-14 10:04:24 +0330

    refactor: remove unused initialization and route files

--- packages/bot/src/lib/initialize-nitrobase.ts
+++
@@ -1,8 +0,0 @@
-import {config} from '../config.js';
-import {nitrobase} from './nitrobase.js';
-
-export function initializeNitrobase() {
-  if (nitrobase.hasStore(config.nitrobase.categoryCollection) === false) {
-    nitrobase.newCollection(config.nitrobase.categoryCollection);
-  }
-}
--- packages/bot/src/lib/server.ts
+++
@@ -1,5 +0,0 @@
-import {NanotronApiServer} from 'alwatr/nanotron';
-
-import {config} from '../config.js';
-
-export const nanotronApiServer = new NanotronApiServer(config.nanotronApiServer);
