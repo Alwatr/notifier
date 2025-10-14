7b03809d2fb1d85e35699df2ad3381cc5b2db21b
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-10-14 11:03:21 +0330

    refactor: add type import for User and enhance openUserCollection function

--- packages/bot/src/lib/nitrobase.ts
+++ packages/bot/src/lib/nitrobase.ts
@@ -2,4 +2,8 @@ import {AlwatrNitrobase} from 'alwatr/nitrobase';
 
 import {config} from '../config.js';
 
+import type {User} from './type.js';
+
 export const nitrobase = /* #__PURE__ */ new AlwatrNitrobase(config.nitrobase.config);
+
+export const openUserCollection = /* #__PURE__ */ () => nitrobase.openCollection<User>(config.nitrobase.usersCollection);
