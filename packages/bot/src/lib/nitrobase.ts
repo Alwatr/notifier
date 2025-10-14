e8d5f7ef24f3f1e26a5c0dc5c77b6a745eff955e
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-10-14 10:04:47 +0330

    refactor: remove unused type import and streamline nitrobase initialization

--- packages/bot/src/lib/nitrobase.ts
+++ packages/bot/src/lib/nitrobase.ts
@@ -2,8 +2,4 @@ import {AlwatrNitrobase} from 'alwatr/nitrobase';
 
 import {config} from '../config.js';
 
-import type {Category} from '../type.js';
-
-export const nitrobase = new AlwatrNitrobase(config.nitrobase.config);
-
-export const openCategoryCollection = async () => await nitrobase.openCollection<Category>(config.nitrobase.categoryCollection);
+export const nitrobase = /* #__PURE__ */ new AlwatrNitrobase(config.nitrobase.config);
