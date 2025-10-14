4c4e2de5a1044b611aed98120c03dcdb153a7450
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-10-14 09:45:21 +0330

    refactor: remove unused imports and clean up config structure

--- packages/bot/src/config.ts
+++ packages/bot/src/config.ts
@@ -1,30 +1,22 @@
 import {getEnv} from '@alwatr/env';
-import {createLogger, packageTracer} from 'alwatr/nanolib';
+import {createLogger} from 'alwatr/nanolib';
 import {Region, StoreFileType, type AlwatrNitrobaseConfig, type StoreFileStat} from 'alwatr/nitrobase';
 
-import type {NanotronApiServerConfig} from 'alwatr/nanotron';
 import type {PollingOptions, ApiClientOptions} from 'grammy';
-
-__dev_mode__: packageTracer.add(__package_name__, __package_version__);
 
 export const logger = /* #__PURE__ */ createLogger(__package_name__);
 
+const initializeMode = getEnv({name: 'initializeMode', defaultValue: ''}) !== '';
+
 export const config = {
-  accessToken: getEnv({name: 'accessToken', developmentValue: 'ADMIN_TOKEN'}),
-
-  nanotronApiServer: {
-    host: getEnv({name: 'host', defaultValue: '0.0.0.0'}),
-    port: +getEnv({name: 'port', defaultValue: '80', developmentValue: '8000'}),
-    prefix: '/api/',
-  } as NanotronApiServerConfig,
-
   nitrobase: {
     config: {
       rootPath: getEnv({name: 'dbPath', developmentValue: './db'}),
-    } as AlwatrNitrobaseConfig,
+      errorWhenNotInitialized: !initializeMode,
+    } as Readonly<AlwatrNitrobaseConfig>,
 
-    categoryCollection: {
-      name: 'categories',
+    userList: {
+      name: 'user-list',
       region: Region.Managers,
       type: StoreFileType.Collection,
     } as StoreFileStat,
