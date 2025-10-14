887b6de3339c4d6f94231e4ef20adb7603bc783c
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-10-14 11:03:14 +0330

    refactor: update message constants and remove unused entries

--- packages/bot/src/lib/message.ts
+++ packages/bot/src/lib/message.ts
@@ -1,10 +1,11 @@
-/* eslint-disable max-len */
-
 export const message = {
-  private_bot_warning: 'این یک بات خصوصی هست! لطفا مزاحم نشوید!',
-  invalid_data_submitted: 'اطلاعات ارسالی نامعتبر است!',
-  success_added_to_list: 'شما با موفقیت به لیست اضافه شدید',
-  already_added_to_list: 'شما قبلا به این لیست اضافه شده اید.',
-  unsubscribe_button_label: 'لغو عضویت از لیست',
-  unsubscribed_from_list: 'شما از لیست {categoryId} حذف شدید',
+  command_available_in_private_chat_only: 'این دستور فقط در چت خصوصی با ربات قابل استفاده است!',
+  new_refer_user: 'کاربر جدیدی به نام {user} با لینک شما وارد ربات شد.',
+  registerSuccess: `
+سلام {firstName} عزیز
+ثبت‌نام شما با موفقیت انجام شد! 🎉
+به دوره خوش آمدید. برای ورود به دوره، لطفاً از لینک زیر استفاده کنید:
+
+https://t.me/ali_mihandoost_ml
+`,
 } as const;
