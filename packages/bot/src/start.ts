762bc0a330f0b5d3231ff070cbd5e90cbcbfa52c
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-10-16 00:43:19 +0330

    fix: reorder import statements for clarity and maintain eslint rules

--- packages/bot/src/start.ts
+++ packages/bot/src/start.ts
@@ -1,7 +1,11 @@
-import './command/just-private.js';
-// eslint-disable-next-line import/order
-import './command/admin.js';
+/* eslint-disable import/order */
+import './command/just-private.js'; // must be first
+import './command/admin.js'; // must be second
+import './command/start-referral.js'; // must before start-normal
 import './command/start-normal.js';
+import './command/update-contact.js';
+import './command/menu.js';
+
 import {config} from './config.js';
 import {startBot} from './lib/bot.js';
 import {logger} from './lib/logger.js';
