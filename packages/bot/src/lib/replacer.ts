d25972a7128ac625c8297e2482b3bc413022a64a
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-10-15 20:51:45 +0330

    fix: update replaceString function to require replaceArgs and handle undefined values

--- packages/bot/src/lib/replacer.ts
+++ packages/bot/src/lib/replacer.ts
@@ -1,8 +1,8 @@
-export function replaceString(template: string, replaceArgs?: DictionaryOpt<string>): string {
+export function replaceString(template: string, replaceArgs?: DictionaryReq<string>): string {
   let result = template;
   if (!replaceArgs) return result;
   for (const [key, value] of Object.entries(replaceArgs)) {
-    result = result.replace(`{${key}}`, value ?? '');
+    result = result.replace(`{${key}}`, value);
   }
   return result;
 }
