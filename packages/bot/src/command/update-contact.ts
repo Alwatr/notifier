98422cd87762bb67f9e7b2ae45bcb69f0461d1b4
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-10-15 20:09:08 +0330

    feat: implement contact message handler to validate and store user contact information

---
+++ packages/bot/src/command/update-contact.ts
@@ -0,0 +1,36 @@
+import {Keyboard} from 'grammy';
+
+import {bot} from '../lib/bot.js';
+import {messages} from '../lib/message.js';
+import {sendMessage} from '../lib/send-message.js';
+import {userCollection} from '../lib/users-collection.js';
+
+bot.on('message:contact', async (ctx) => {
+  const {chat, from, message} = ctx;
+  if (!from || !message.contact) {
+    return;
+  }
+
+  const contact = message.contact;
+  const vars = {
+    name: `${from.first_name} ${from.last_name ?? ''}`.trim(),
+  };
+
+  // Ensure the contact is from the user themselves
+  if (contact.user_id !== from.id || !contact.phone_number) {
+    ctx.reply(messages.invalid_contact_text, {
+      reply_markup: new Keyboard().requestContact(messages.request_contact_keyboard_text).resized().oneTime(),
+    });
+    return;
+  }
+
+  userCollection.mergeItemData(chat.id, {
+    phone: contact.phone_number,
+  });
+
+  await sendMessage({
+    chatId: chat.id,
+    messages: messages.register_success,
+    vars,
+  });
+});
