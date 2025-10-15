56dd13075ddefa474790c5280b56ef2c24bb7372
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-10-15 20:08:50 +0330

    feat: add replaceString function for template string replacement

---
+++ packages/bot/src/lib/replacer.ts
@@ -0,0 +1,8 @@
+export function replaceString(template: string, replaceArgs?: DictionaryOpt<string>): string {
+  let result = template;
+  if (!replaceArgs) return result;
+  for (const [key, value] of Object.entries(replaceArgs)) {
+    result = result.replace(`{${key}}`, value ?? '');
+  }
+  return result;
+}
