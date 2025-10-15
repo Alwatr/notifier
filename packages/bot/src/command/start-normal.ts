efb284d28a1815a6fef893541e7f878c5ffd4afb
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-10-15 20:09:18 +0330

    feat: implement start command to handle user registration and request contact information

---
+++ packages/bot/src/command/start-normal.ts
@@ -0,0 +1,62 @@
+import {Keyboard} from 'grammy';
+
+import {bot} from '../lib/bot.js';
+import {logger} from '../lib/logger.js';
+import {messages} from '../lib/message.js';
+import {sendMessage} from '../lib/send-message.js';
+import {userCollection} from '../lib/users-collection.js';
+
+bot.command(
+  'start',
+
+  /**
+   * Normal start command without parameters.
+   */
+  async (ctx, next) => {
+    const {chat, from} = ctx;
+
+    if (ctx.match || !from) {
+      return next();
+    }
+
+    logger.logMethodArgs?.('command_start_normal', chat);
+
+    const vars = {
+      name: `${from.first_name} ${from.last_name ?? ''}`.trim(),
+    };
+
+    try {
+      if (userCollection.hasItem(from.id) && userCollection.getItemData(from.id).phone) {
+        await sendMessage({
+          chatId: chat.id,
+          messages: messages.already_registered,
+          vars,
+        });
+        return;
+      }
+      // else
+
+      if (!userCollection.hasItem(from.id)) {
+        // Save user data
+        userCollection.addItem(from.id, {
+          id: from.id,
+          firstName: from.first_name,
+          lastName: from.last_name ?? '',
+          username: from.username ?? null,
+          phone: null,
+          invitedBy: null,
+          referralCount: 0,
+        });
+      }
+
+      await sendMessage({
+        chatId: chat.id,
+        messages: messages.request_contact,
+        reply_markup: new Keyboard().requestContact(messages.request_contact_keyboard_text).resized().oneTime(),
+      });
+    }
+    catch (error) {
+      logger.error?.('startCommand', 'unexpected_error', error, {chat, from});
+    }
+  },
+);
