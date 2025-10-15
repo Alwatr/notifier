1eb684ca3c2a19b614f4ddd33df05223d2e3c384
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-10-15 20:09:32 +0330

    refactor: remove inspect command implementation from bot

--- packages/bot/src/command/inspect.ts
+++
@@ -1,7 +0,0 @@
-import {bot} from '../lib/bot.js';
-import {logger} from '../lib/logger.js';
-
-bot.on('message', async (ctx, next) => {
-  logger.logMethodArgs?.('message', ctx.message);
-  next();
-});
