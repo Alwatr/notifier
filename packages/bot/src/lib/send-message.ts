595358e97f64ea744c9832371e422e9b2436259c
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-10-15 20:08:40 +0330

    refactor: restructure sendMessage function to use options object and improve message handling

--- packages/bot/src/lib/send-message.ts
+++ packages/bot/src/lib/send-message.ts
@@ -1,22 +1,35 @@
 import {bot} from './bot.js';
 import {logger} from './logger.js';
+import {replaceString} from './replacer.js';
 
 import type {MsgItem} from './message.js';
+import type {ForceReply, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove} from 'grammy/types';
 
-export async function sendMessage(chatId: number, msgs: MsgItem[]) {
-  for (const msg of msgs) {
+type sendMessageOptions = {
+  chatId: number;
+  messages: MsgItem[];
+  vars?: DictionaryOpt<string>;
+  reply_markup?: InlineKeyboardMarkup | ReplyKeyboardMarkup | ReplyKeyboardRemove | ForceReply;
+};
+export async function sendMessage(options: sendMessageOptions) {
+  for (const msg of options.messages) {
     try {
       switch (msg.type) {
         case 'text':
-          await bot.api.sendMessage(chatId, msg.text);
+          await bot.api.sendMessage(options.chatId, replaceString(msg.text, options.vars), {
+            reply_markup: options.reply_markup,
+            parse_mode: 'MarkdownV2',
+          });
           break;
         case 'simple':
-          await bot.api.copyMessage(chatId, msg.fromChatId, msg.messageId);
+          await bot.api.copyMessage(options.chatId, msg.fromChatId, msg.messageId, {
+            reply_markup: options.reply_markup,
+          });
           break;
       }
     }
     catch (error) {
-      logger.error?.('sendMessage', 'unexpected_error', error, {chatId, msgs});
+      logger.error?.('sendMessage', 'unexpected_error', error, {chatId: options.chatId, msgs: options.messages});
     }
   }
 }
