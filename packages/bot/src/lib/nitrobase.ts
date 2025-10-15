4845311b3e4a7e7bbd76c265cb69aac15f6d2566
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-10-15 20:08:34 +0330

    refactor: remove unused User type import from nitrobase module

--- packages/bot/src/lib/nitrobase.ts
+++ packages/bot/src/lib/nitrobase.ts
@@ -2,8 +2,4 @@ import {AlwatrNitrobase} from 'alwatr/nitrobase';
 
 import {config} from '../config.js';
 
-import type {User} from './type.js';
-
 export const nitrobase = /* #__PURE__ */ new AlwatrNitrobase(config.nitrobase.config);
-
-export const openUserCollection = /* #__PURE__ */ () => nitrobase.openCollection<User>(config.nitrobase.usersCollection);
