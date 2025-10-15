14ad143fc40768ec1a45eb11cbedc958e3c5bc04
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-10-15 21:29:13 +0330

    feat: add menu items and main menu keyboard layout

---
+++ packages/bot/src/lib/menu.ts
@@ -0,0 +1,24 @@
+import {Keyboard} from 'grammy';
+
+export const menuItems = {
+  enterGroup: 'ورود به سالن اصلی 🎻',
+  courseInfo: 'اطلاعات کامل دوره ℹ️',
+
+  inviteFriends: 'دعوت از دوستان 🎁',
+  stats: 'آمار دعوت‌های شما 📊',
+
+  scheduleMeetings: 'زمان‌بندی جلسات 🗓️',
+  support: 'پشتیبانی 💬',
+};
+
+export const mainMenu = new Keyboard()
+  .text(menuItems.enterGroup)
+  .text(menuItems.courseInfo)
+  .row() // line break
+  .text(menuItems.inviteFriends)
+  .text(menuItems.stats)
+  .row() // line break
+  .text(menuItems.scheduleMeetings)
+  .text(menuItems.support)
+  .resized()
+  .persistent();
