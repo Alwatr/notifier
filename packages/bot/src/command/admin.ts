1da937c6502e0d3a97477e29d06641e7d8d22fec
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-10-15 20:51:25 +0330

    feat: implement admin commands for notifying users

---
+++ packages/bot/src/command/admin.ts
@@ -0,0 +1,69 @@
+import {config} from '../config.js';
+import {bot} from '../lib/bot.js';
+import {logger} from '../lib/logger.js';
+import {replaceString} from '../lib/replacer.js';
+import {userCollection} from '../lib/users-collection.js';
+
+let currentAdminCommand = '';
+
+bot.command('notify_all', async (ctx) => {
+  const {chat, from} = ctx;
+
+  logger.logMethodArgs?.('command_notify_all', chat);
+
+  if (from?.username !== config.adminUserName) {
+    return;
+  }
+
+  currentAdminCommand = 'notify_all';
+});
+
+bot.command('notify_noref', async (ctx) => {
+  const {chat, from} = ctx;
+
+  logger.logMethodArgs?.('command_notify_noref', chat);
+
+  if (from?.username !== config.adminUserName) {
+    return;
+  }
+
+  currentAdminCommand = 'notify_noref';
+});
+
+// notify_all
+bot.on('message', async (ctx, next) => {
+  const {chat, from, message} = ctx;
+  logger.logMethodArgs?.('message_notify_all', message);
+
+  if (from?.username !== config.adminUserName || !currentAdminCommand.startsWith('notify_')) {
+    return next();
+  }
+  // else
+
+  currentAdminCommand = '';
+
+  for (const user of userCollection.items()) {
+    if (currentAdminCommand === 'notify_noref' && user.data.referralCount > 0) {
+      continue;
+    }
+    try {
+      const vars = {
+        name: `${user.data.firstName} ${user.data.lastName ?? ''}`.trim(),
+        referralCount: user.data.referralCount.toString(),
+        inviteLink: `https://t.me/${config.telegramBot.username}?start=ref_${user.data.id}`,
+      };
+      if (message.text) {
+        await bot.api.sendMessage(user.data.id, replaceString(message.text, vars));
+      }
+      else if (message.caption) {
+        await bot.api.copyMessage(user.data.id, chat.id, message.message_id, {caption: replaceString(message.caption, vars)});
+      }
+      else {
+        await bot.api.copyMessage(user.data.id, chat.id, message.message_id);
+      }
+    }
+    catch (error) {
+      logger.error('message_notify_all', 'send_message_failed', error, {user, message});
+    }
+  }
+});
