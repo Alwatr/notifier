e3ae2755741be17709cb289f6a6b8c1da6626c2b
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-10-16 14:07:22 +0330

    fix: reorder menu items for better organization and clarity

--- packages/bot/src/lib/menu.ts
+++ packages/bot/src/lib/menu.ts
@@ -4,24 +4,25 @@ export const menuItems = {
   enter_group: 'ورود به گروه دوره 🎵',
   course_info: 'اطلاعات کامل دوره ℹ️',
 
-  gift: 'هدیه ویژه 🎁',
-  invite_templates: 'پیام‌های آماده دعوت ✨',
-  stats: 'مشاهده آمار دعوتی‌ها 📊',
-
   schedule_meetings: 'زمان‌بندی جلسات 🗓️',
   support: 'پشتیبانی 💬',
+
+  gift: 'هدیه ویژه 🎁',
+
+  invite_templates: 'پیام‌های آماده دعوت ✨',
+  stats: 'مشاهده آمار دعوتی‌ها 📊',
 } as const;
 
 export const mainMenu = new Keyboard()
   .text(menuItems.course_info)
   .text(menuItems.enter_group)
   .row() // line break
-  .text(menuItems.invite_templates)
-  .text(menuItems.gift)
-  .row() // line break
-  .text(menuItems.stats)
-  .row() // line break
   .text(menuItems.support)
   .text(menuItems.schedule_meetings)
+  .row() // line break
+  .text(menuItems.gift)
+  .row() // line break
+  .text(menuItems.invite_templates)
+  .text(menuItems.stats)
   .resized()
   .persistent();
