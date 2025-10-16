bb1741dae063411abce25ce24b2ceb840a682a3b
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-10-16 14:06:35 +0330

    fix: ensure proper handling of non-private chat messages and improve contact validation logic

--- packages/bot/src/command/just-private.ts
+++ packages/bot/src/command/just-private.ts
@@ -2,15 +2,29 @@ import {bot} from '../lib/bot.js';
 import {logger} from '../lib/logger.js';
 import {messages} from '../lib/message.js';
 import {sendMessage} from '../lib/send-message.js';
+import {userCollection} from '../lib/users-collection.js';
 
 bot.on('message', async (ctx, next) => {
   logger.logMethodArgs?.('message', ctx.message);
+
   if (ctx.chat.type !== 'private') {
     logger.incident?.('startCommand', 'invalid_chat_type');
-    return sendMessage({
+    await sendMessage({
       chatId: ctx.chat.id,
       messages: messages.private_chat_only,
     });
+    return;
   }
+  // else
+
+  if (userCollection.hasItem(ctx.from.id) && userCollection.getItemData(ctx.from.id).phone === null && !ctx.message.contact) {
+    await sendMessage({
+      chatId: ctx.chat.id,
+      messages: messages.invalid_contact,
+    });
+    return;
+  }
+  // else
+
   next();
 });
