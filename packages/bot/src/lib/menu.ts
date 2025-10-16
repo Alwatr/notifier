655403cad64535050db8d22c691ea092caca45ab
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-10-16 11:42:56 +0330

    fix: reorder menu items to prioritize gift option over invite templates

--- packages/bot/src/lib/menu.ts
+++ packages/bot/src/lib/menu.ts
@@ -5,6 +5,7 @@ export const menuItems = {
   course_info: 'اطلاعات کامل دوره ℹ️',
 
   gift: 'هدیه ویژه 🎁',
+  invite_templates: 'پیام‌های آماده دعوت ✨',
   stats: 'مشاهده آمار دعوتی‌ها 📊',
 
   schedule_meetings: 'زمان‌بندی جلسات 🗓️',
@@ -15,8 +16,10 @@ export const mainMenu = new Keyboard()
   .text(menuItems.course_info)
   .text(menuItems.enter_group)
   .row() // line break
-  .text(menuItems.stats)
+  .text(menuItems.invite_templates)
   .text(menuItems.gift)
+  .row() // line break
+  .text(menuItems.stats)
   .row() // line break
   .text(menuItems.support)
   .text(menuItems.schedule_meetings)
