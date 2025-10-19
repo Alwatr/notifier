9f0245bd4817e89af2d6fda744f627c37b13a29d
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-10-19 16:38:51 +0330

    fix: refactor message handling to improve user contact validation

--- packages/bot/src/command/just-private.ts
+++ packages/bot/src/command/just-private.ts
@@ -1,28 +1,38 @@
 import {bot} from '../lib/bot.js';
 import {logger} from '../lib/logger.js';
-import {messages} from '../lib/message.js';
+import {messages, userDeclineContactMessage} from '../lib/message.js';
 import {sendMessage} from '../lib/send-message.js';
 import {userCollection} from '../lib/users-collection.js';
 
 bot.on('message', async (ctx, next) => {
-  logger.logMethodArgs?.('message', ctx.message);
+  const {chat, from, message} = ctx;
+  logger.logMethodArgs?.('message', message);
 
-  if (ctx.chat.type !== 'private') {
-    logger.incident?.('startCommand', 'invalid_chat_type');
-    await sendMessage({
-      chatId: ctx.chat.id,
-      messages: messages.private_chat_only,
-    });
-    return;
-  }
-  // else
+  if (
+    userCollection.hasItem(from.id) && // existing user
+    userCollection.getItemData(from.id).phone === null && // no contact info
+    !message.contact // no contact info shared in this message
+  ) {
+    if (message.text === userDeclineContactMessage) {
+      userCollection.mergeItemData(chat.id, {
+        phone: '',
+      });
 
-  if (userCollection.hasItem(ctx.from.id) && userCollection.getItemData(ctx.from.id).phone === null && !ctx.message.contact) {
-    await sendMessage({
-      chatId: ctx.chat.id,
-      messages: messages.invalid_contact,
-    });
-    return;
+      await sendMessage({
+        chatId: chat.id,
+        messages: messages.register_success,
+        vars: {
+          name: `${from.first_name} ${from.last_name ?? ''}`.trim(),
+        },
+      });
+    }
+    else {
+      await sendMessage({
+        chatId: chat.id,
+        messages: messages.invalid_contact,
+      });
+      return;
+    }
   }
   // else
 
